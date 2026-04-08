23d4492393fc0085a834a73841634d8894e24ffd
Jose Rivera <jriverar1@unicartagena.edu.co>
2026-04-08 11:33:00 -0500

    feat(miniproyecto: realizacion del miniproyecto en C#)

--- Program.cs
+++ Program.cs
@@ -1,54 +1,56 @@
 ﻿using System;
-namespace ActividadU1
+namespace ProyectoFinal
 {
+    //Clase que contine matriz//
     public class Estudiante
     {
         public string Nombre { get; set; }
-        public int Edad { get; set; }
-        public double Promedio { get; private set; } 
-        // Constructor
-        public Estudiante(string nombre, int edad, double promedio)
+        // Matriz bidimensional//
+        public double[,] Calificaciones { get; set; }
+        public Estudiante(string nombre, double[,] calificaciones)
         {
             Nombre = nombre;
-            Edad = edad;
-            Promedio = promedio;
+            Calificaciones = calificaciones;
         }
-        // Método solicitado: mostrarInfo//
-        public void MostrarInfo()
+        // Metodo para imprimir la matriz de forma ordenada//
+        public void MostrarBoleta()
         {
-            Console.WriteLine($"[Estudiante] Nombre: {Nombre} | Edad: {Edad} | Promedio: {Promedio}");
-        }
-        // Método solicitado: setPromedio//
-        public void SetPromedio(double nuevoPromedio)
-        {
-            Promedio = nuevoPromedio;
-            Console.WriteLine($"\n>> Sistema: El promedio de {Nombre} se actualizó a {Promedio}\n");
+            Console.WriteLine($"\nBoleta de: {Nombre}"); 
+            // Recorrer la matriz://
+            for (int fila = 0; fila < Calificaciones.GetLength(0); fila++)
+            {
+                Console.Write($"Materia {fila + 1}: ");
+                for (int col = 0; col < Calificaciones.GetLength(1); col++)
+                {
+                    Console.Write($"[{Calificaciones[fila, col]}] ");
+                }
+                Console.WriteLine(); // Salto de línea al terminar la materia
+            }
         }
     }
-    // Ejecucion principal//
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Objetos y Clases\n");
-            var estudiante1 = new Estudiante("Juanjo", 20, 8.5);
-            var estudiante2 = new Estudiante("Peya", 22, 9.0);
-            var estudiante3 = new Estudiante("Messi", 21, 7.5);
-            //arreglo//
-            Estudiante[] listaEstudiantes = { estudiante1, estudiante2, estudiante3 };
-            //Recorrido llamando al metodo mostrarInfo()//
-            Console.WriteLine("Datos Originales");
-            foreach (var est in listaEstudiantes)
+            Console.WriteLine("MiniProyecto: Arreglo de objetos con matriz en C#");
+            // se crean las matrices//
+            double[,] notasJuanjo = { 
+                { 8.5, 9.0, 9.5 }, 
+                { 7.0, 8.0, 7.5 }  
+            };
+            double[,] notasPeya = { 
+                { 9.0, 9.5, 10.0 }, 
+                { 8.5, 8.0, 9.0 }   
+            };
+            // se crean los objetos con su respectiva matriz//
+            Estudiante est1 = new Estudiante("Juanjo", notasJuanjo);
+            Estudiante est2 = new Estudiante("Peya", notasPeya);
+            //arreglo de objetos//
+            Estudiante[] grupoEstudiantes = { est1, est2 };
+            //Recorrer el arreglo y llamar a su metodo//
+            foreach (var alumno in grupoEstudiantes)
             {
-                est.MostrarInfo();
-            }
-            //Modificación usando el método setPromedio()//
-            listaEstudiantes[0].SetPromedio(9.8);
-            // Mostramos cómo quedó la lista tras el cambio
-            Console.WriteLine("Datos Actualizados");
-            foreach (var est in listaEstudiantes)
-            {
-                est.MostrarInfo();
+                alumno.MostrarBoleta();
             }
         }
     }
