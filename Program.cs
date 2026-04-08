c247544ca967c3a66b151fd7a127ef44b62307e0
Jose Rivera <jriverar1@unicartagena.edu.co>
2026-04-08 10:31:35 -0500

    feat(Objetos: se implementan la clase estudiantes e instancias )

--- Program.cs
+++ Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 namespace ActividadU1
 {
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public double Promedio { get; private set; } 
+        // Constructor
+        public Estudiante(string nombre, int edad, double promedio)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Promedio = promedio;
+        }
+        // Método solicitado: mostrarInfo//
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"[Estudiante] Nombre: {Nombre} | Edad: {Edad} | Promedio: {Promedio}");
+        }
+        // Método solicitado: setPromedio//
+        public void SetPromedio(double nuevoPromedio)
+        {
+            Promedio = nuevoPromedio;
+            Console.WriteLine($"\n>> Sistema: El promedio de {Nombre} se actualizó a {Promedio}\n");
+        }
+    }
+    // Ejecucion principal//
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Structs");
-            var s1 = new EstudianteStruct("Juanjo", 20, 8.5);
-            var s2 = new EstudianteStruct("Peya", 22, 9.0);
-            var s3 = new EstudianteStruct("Messi", 21, 7.5);
-            var lista = new[] { s1, s2, s3 };
-            foreach (var s in lista)
+            Console.WriteLine("Objetos y Clases\n");
+            var estudiante1 = new Estudiante("Juanjo", 20, 8.5);
+            var estudiante2 = new Estudiante("Peya", 22, 9.0);
+            var estudiante3 = new Estudiante("Messi", 21, 7.5);
+            //arreglo//
+            Estudiante[] listaEstudiantes = { estudiante1, estudiante2, estudiante3 };
+            //Recorrido llamando al metodo mostrarInfo()//
+            Console.WriteLine("Datos Originales");
+            foreach (var est in listaEstudiantes)
             {
-                Console.WriteLine($"{s.Nombre} - {s.Promedio}");
+                est.MostrarInfo();
             }
-            lista[0].Promedio = 9.5;
-            Console.WriteLine("\nDespués de modificar:");
-            foreach (var s in lista)
+            //Modificación usando el método setPromedio()//
+            listaEstudiantes[0].SetPromedio(9.8);
+            // Mostramos cómo quedó la lista tras el cambio
+            Console.WriteLine("Datos Actualizados");
+            foreach (var est in listaEstudiantes)
             {
-                Console.WriteLine($"{s.Nombre} - {s.Promedio}");
+                est.MostrarInfo();
             }
         }
     }
