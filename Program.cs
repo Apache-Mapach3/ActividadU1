002973f082fb605401eccc84da9da13d150c3932
Jose Rivera <jriverar1@unicartagena.edu.co>
2026-04-08 09:26:52 -0500

    feat(strutct): Ejercicio de struct realizado en C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ActividadU1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Structs");
+            var s1 = new EstudianteStruct("Juanjo", 20, 8.5);
+            var s2 = new EstudianteStruct("Peya", 22, 9.0);
+            var s3 = new EstudianteStruct("Messi", 21, 7.5);
+            var lista = new[] { s1, s2, s3 };
+            foreach (var s in lista)
+            {
+                Console.WriteLine($"{s.Nombre} - {s.Promedio}");
+            }
+            lista[0].Promedio = 9.5;
+            Console.WriteLine("\nDespués de modificar:");
+            foreach (var s in lista)
+            {
+                Console.WriteLine($"{s.Nombre} - {s.Promedio}");
+            }
+        }
+    }
+}
